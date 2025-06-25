1942ed4443ab03ea7da75adec25b66bbc4ed23db
PRAGYA KUMARI MISHRA <100357141+pragyakuumarimishra@users.noreply.github.com>
2025-06-25 18:08:39 +0530

    Add files via upload

---
+++ JAVASCRIPT/Hello.js
@@ -0,0 +1,79 @@
+// console.log("Hello, World!");
+// console.log("My name is Pragya.");
+// console.log(7+3);
+// var message;
+// message="Hello geeks World!";
+// console.log(message);
+// console.log(message);
+// message="Hello geeks for geeks";
+// console.log(message);
+// let text;//creating a variable
+// text = "javascript";//assigning a value to the variable
+// console.log(text);
+
+// const number=3;
+
+// console.log(number);
+// var message="hii";
+// console.log(message);
+
+// var message="hie";
+// var message="hi";
+// console.log(message);
+
+// let message="hii";
+// message="hie";
+// message="hi";
+// console.log(typeof message);
+   
+// let isproductincart=null;
+//  console.log(typeof isproductincart); 
+// const personDetails={
+//     name: "Pragya",
+//     age: 25,
+//     isStudent: true,
+  
+    
+// }
+// const Sync = require('readline-sync');
+// const userName=Sync.question("What is your name? ");
+// //console.log("Hello, " + name);
+//  console.log(`Hello ${userName} ,welcome to gfg`);
+//  console.log("A" <"B");
+//  console.log(null==undefined);
+//    console.log(null===undefined);
+//     console.log(null===0);
+//     console.log(null>=0);
+//     console.log(null<1);
+// const readline = require('readline-sync');
+// const num= Number(readline.question("enter the number? "));
+ 
+//  const remOf3= num%3;
+//   const remOf5= num%5;
+// if(remOf3===0 && remOf5===0){
+//      console.log("fizz");
+//   }
+//    if(remOf5==0 || remOf3===0){
+//    console.log("buzz");  
+// }
+//     else{
+//          console.log("This is false");
+//     }
+// const firstName = "pragya";
+// const nickName = null;
+// //console.log(typeof firstName);
+// let name = firstName && nickName ;
+// console.log(`name is ${name}`);
+// for(let i=0; i<=100; i++){
+//     console.log(i);
+// }
+
+//  let userName = "Pragya";
+//  console.log(userName[0]); 
+  for(let i=1; i<=10; i++){
+    let row = "";
+    for(let j=1; j<=i; j++){
+    row+= "*";
+  }
+  console.log(row);
+  }
